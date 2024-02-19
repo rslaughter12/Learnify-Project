9bcc4a9809302c70abbcfc7789b1cf9b84e68cc1
rslaughter12 <rslaughter12@gmail.com>
2024-02-19 14:47:57 -0800

    onemoretime

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI;
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb+srv://racevedoslaughter12:323585rs@cluster0.6wdkwmq.mongodb.net/LEARNIFY_DB?retryWrites=true&w=majority'
