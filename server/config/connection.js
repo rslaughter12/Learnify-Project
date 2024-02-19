53fe8062e164c5e87864bde38117a0f88bffed58
rslaughter12 <rslaughter12@gmail.com>
2024-02-19 13:45:49 -0800

    atlas connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/LEARNIFY_DB'
+  process.env.MONGODB_URI || 'mongodb+srv://racevedoslaughter12:323585rs@cluster0.6wdkwmq.mongodb.net/LEARNIFY_DB?retryWrites=true&w=majority'
 );
 
 module.exports = mongoose.connection;
