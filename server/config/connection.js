9b5320ce8a626d771e75bbbce3b2f0a252707983
rslaughter12 <rslaughter12@gmail.com>
2024-02-19 14:47:32 -0800

    update to link

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI;
 
 mongoose.connect(
-  process.env.MONGODB_URI || uri,
+  process.env.MONGODB_URI || 'mongodb+srv://racevedoslaughter12:323585rs@cluster0.6wdkwmq.mongodb.net/LEARNIFY_DB?retryWrites=true&w=majority'
 );
 
 module.exports = mongoose.connection;
