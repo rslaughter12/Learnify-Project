82909ad1049cc25a31a3369f4b595d60f90fa557
rslaughter12 <rslaughter12@gmail.com>
2024-02-19 14:42:37 -0800

    more var work

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI;
 
 mongoose.connect(
-  uri
+  process.env.MONGODB_URI || uri,
 );
 
 module.exports = mongoose.connection;
